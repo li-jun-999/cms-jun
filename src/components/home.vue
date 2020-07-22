73907fa33ad85621d1d9b91169e6f2b9b4638cd1
李军军 <juli@bplead.com>
2020-07-22 15:34:19 +0800

    为页面添加九宫格

--- src/components/home.vue
+++ src/components/home.vue
@@ -7,17 +7,52 @@
                 <img :src="lunbotu.url" alt="">
             </mt-swipe-item>
         </mt-swipe>
+
+        <!-- 设置图片九宫格 -->
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img :src="menu1">
+		                    <div class="mui-media-body">新闻资讯</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img :src="menu2">
+		                    <div class="mui-media-body">图片分享</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img :src="menu3">
+		                    <div class="mui-media-body">商品购买</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img :src="menu4">
+		                    <div class="mui-media-body">留言反馈</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img :src="menu5">
+		                    <div class="mui-media-body">视频专区</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img :src="menu6">
+		                    <div class="mui-media-body">联系我们</div></a></li>
+		        </ul> 
     </div>
 </template>
 
 <script>
 //按需导入
 import {Toast} from 'mint-ui'
+//导入图片
+import menu1 from '../images/menu1.png'
+import menu2 from '../images/menu2.png'
+import menu3 from '../images/menu3.png'
+import menu4 from '../images/menu4.png'
+import menu5 from '../images/menu5.png'
+import menu6 from '../images/menu6.png'
 
 export default {
     data() {
         return {
-            lunbotus:[]
+            lunbotus:[], //存放轮播图的数组
+            menu1:menu1,//解决src引入图片不显示的问题
+            menu2:menu2,
+            menu3:menu4,
+            menu4:menu4,
+            menu5:menu5,
+            menu6:menu6,
         }
     },
     //生命周期内获取数据
@@ -67,4 +102,15 @@ export default {
         }
     }
 
+    .mui-grid-view.mui-grid-9 {
+        background-color: white;
+        img {
+            width: 60px;
+            height: 60px;
+        }
+    }
+    .mui-grid-view.mui-grid-9 .mui-table-view-cell {
+        border-right: 0ch;
+        border-bottom: none;
+    }
 </style>
